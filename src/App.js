6539b4428aacec1f40012fa367bb3f057aa9cb32
GanYihuan <583520052@qq.com>
2019-06-09 21:54:34 +0800

    pure react project init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import {
+  Route,
+  Switch
+} from 'react-router-dom'
+// import Register from './container/register/register'
+// import AuthRoute from './component/authroute/authroute'
+// import GeniusInfo from './container/geniusinfo/geniusinfo'
+// import Login from './container/login/login'
+// import Dashboard from './component/dashboard/dashboard'
+// import BossInfo from './container/bossinfo/bossinfo'
+// import Chat from './component/chat/chat'
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        App
+      </div>
+    )
+  }
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import reducers from './reducer'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducers, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+))
+
+ReactDOM.render(
+  (<Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>),
+  document.getElementById('root')
+)
