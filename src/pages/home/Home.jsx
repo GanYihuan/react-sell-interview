ef9e661befe5497c8b493be39a0794eeb6a96517
GanYihuan <583520052@qq.com>
2019-06-10 01:05:29 +0800

    header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@
 import { connect } from 'react-redux'
 import { Route, withRouter } from 'react-router-dom' // withRouter: 能在组件调用 push 方法
 import Loadable from 'react-loadable' // 封装懒加载操作
-import BottomBar from './components/BottomBar'
+import BottomBar from './components/BottomBar/BottomBar'
+import Header from './components/Header/Header'
 
 class Home extends React.Component {
   render() {
     return (
       <div>
+        <Header/>
         <BottomBar/>
       </div>
     )
---
+++ src/pages/home/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+﻿import React from 'react'
+import './Header.scss'
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className='header'>
+        <img className='banner-img' src='//xs01.meituan.net/waimai_i/img/bannertemp.e8a6fa63.jpg'/>
+      </div>
+    )
+  }
+}
+
+export default Header
