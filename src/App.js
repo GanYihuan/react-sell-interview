b7e81a6c4ff7ad9d4733fc7befe19e1f882090b2
GanYihuan <583520052@qq.com>
2019-06-10 05:50:41 +0800

    lazy & Suspense

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component, lazy, Suspense } from 'react'
 import { Provider } from 'react-redux'
-import { BrowserRouter, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import store from './store'
-import Home from './pages/home/Home'
-import My from './pages/my/My'
+
+const Home = lazy(() => import(/* webpackChunkName: "Home" */ './pages/home/Home'))
+const My = lazy(() => import(/* webpackChunkName: "My" */ './pages/my/My'))
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <BrowserRouter>
-          <div>
+          <Suspense fallback={<div>Loading...</div>}>
             <Redirect to= '/home' />
-            <Route path='/home' exact component={Home} />
-            <Route path='/my' exact component={My} />
-            <Route path='/order' exact component={My} />
-          </div>
+            <Switch>
+              <Route path='/home' exact component={Home} />
+              <Route path='/my' exact component={My} />
+              <Route path='/order' exact component={My} />
+            </Switch>
+          </Suspense>
         </BrowserRouter>
       </Provider>
     )
