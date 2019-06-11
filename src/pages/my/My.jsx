957fed8e0a179c56978403b3bdc8db2c96be3c4f
GanYihuan <583520052@qq.com>
2019-06-12 04:12:05 +0800

    my page

--- src/pages/my/My.jsx
+++ src/pages/my/My.jsx
@@ -1,9 +1,34 @@
 ﻿import React from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { Layout } from 'element-react'
 import BottomBar from '../../common/BottomBar/BottomBar'
+import 'element-theme-default'
 import './My.scss'
 
 class My extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: '默认用户名',
+      email: ''
+    }
+    this.request = this.request.bind(this)
+  }
+  componentDidMount() {
+    this.request()
+  }
+  async request() {
+    const { status, data: { user, email }} = await axios.get('/users/getUser')
+    if (status === 200) {
+      this.setState(() => {
+        return {
+          user: user,
+          email: email
+        }
+      })
+    }
+  }
   render() {
     return (
       <div>
@@ -12,16 +37,32 @@ class My extends React.Component {
             <Link to={'/login'}>
               <img className='avatar'/>
             </Link>
-            <p className='nickname'>美团小骑手</p>
+            <p className='nickname'>{this.state.user}</p>
           </div>
           <div className='content'>
             <ul className='items'>
-              <li className='address'>收货地址管理</li>
-              <li className='money'>商家代金券</li>
+              <li className='address'>
+                <Layout.Row>
+                  <Layout.Col span='5'>
+                    你的用户名
+                  </Layout.Col>
+                  <Layout.Col span='5' offset='1'>
+                    {this.state.user}
+                  </Layout.Col>
+                </Layout.Row>
+              </li>
             </ul>
             <ul className='items'>
-              <li className='email'>意见反馈</li>
-              <li className='question'>常见问题</li>
+              <li className='address'>
+                <Layout.Row>
+                  <Layout.Col span='5'>
+                    Email 地址
+                  </Layout.Col>
+                  <Layout.Col span='5' offset='1'>
+                    {this.state.email}
+                  </Layout.Col>
+                </Layout.Row>
+              </li>
             </ul>
             <p className='tel'>客服电话:&nbsp;101-097-77</p>
             <p className='time'>服务时间:&nbsp;9:00-23:00</p>
