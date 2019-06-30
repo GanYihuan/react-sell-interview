02350555fbe1421edf1c5b55e98a253032d6c0f7
GanYihuan <583520052@qq.com>
2019-06-30 09:42:20 +0800

    ready change ratingData

--- src/pages/rating/store/actionCreators.js
+++ src/pages/rating/store/actionCreators.js
@@ -1,17 +1,32 @@
 ﻿import axios from 'axios'
+import * as constants from './constants'
 
 export const getCommentData = () => async(dispatch) => {
-  const resp = await axios({
-    method: 'get',
-    url: '/api/comments.json'
-  })
-  dispatch({
-    type: 'COMMENT_LIST_DATA',
-    obj: resp.data
-  })
+  const { status, data: { ratings }} = await axios.get('/ratings/getRating')
+  if (status === 200) {
+    dispatch({
+      type: constants.COMMENT_LIST_DATA,
+      obj: ratings
+    })
+  }
+  // const resp = await axios({
+  //   method: 'get',
+  //   url: '/api/comments.json'
+  // })
+  // dispatch({
+  //   type: 'COMMENT_LIST_DATA',
+  //   obj: resp.data
+  // })
 }
 
 export const getRatingsData = () => async(dispatch) => {
+  // const { status, data: { sellers }} = await axios.get('/sellers/getSellers')
+  // if (status === 200) {
+  //   dispatch({
+  //     type: constants.RATING_DATA,
+  //     obj: sellers
+  //   })
+  // }
   const resp = await axios({
     method: 'get',
     url: '/api/ratings.json'
--- src/pages/rating/store/reducer.js
+++ src/pages/rating/store/reducer.js
@@ -1,4 +1,5 @@
 ﻿import { fromJS } from 'immutable'
+import * as constants from './constants'
 
 const defaultState = fromJS({
   commentData: {},
@@ -7,23 +8,25 @@ const defaultState = fromJS({
 
 export default (state = defaultState, action) => {
   switch (action.type) {
-    case 'COMMENT_LIST_DATA':
-      return getListData(state, action)
-    case 'RATING_DATA':
+    case constants.COMMENT_LIST_DATA:
+      return getCommentListData(state, action)
+    case constants.RATING_DATA:
       return getRatingtData(state, action)
     default:
       return state
   }
 }
 
-const getListData = (state, action) => {
+const getCommentListData = (state, action) => {
   return state.merge({
-    commentData: state.get('commentData').concat(fromJS(action.obj.data)) // imutable obj, use get()
+    // commentData: state.get('commentData').concat(fromJS(action.obj.data)) // imutable obj, use get()
+    commentData: state.get('commentData').concat(fromJS(action.obj)) // imutable obj, use get()
   })
 }
 
 const getRatingtData = (state, action) => {
   return state.merge({
     ratingData: state.get('ratingData').concat(fromJS(action.obj.data)) // imutable obj, use get()
+    // ratingData: state.get('ratingData').concat(fromJS(action.obj)) // imutable obj, use get()
   })
 }
