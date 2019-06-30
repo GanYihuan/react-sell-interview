8c331edeb33d5dfbd862960a1c42a4be4d7af813
GanYihuan <583520052@qq.com>
2019-06-30 21:31:17 +0800

    NavHeader mongodb ready

--- src/pages/Menu/store/reducer.js
+++ src/pages/Menu/store/reducer.js
@@ -1,5 +1,5 @@
 ﻿import { fromJS } from 'immutable'
-import cartControl from '../components/cartControl/cartControl'
+import * as constants from './constants'
 
 const initState = fromJS({
   menuData: [],
@@ -29,12 +29,6 @@ export default (state = initState, action) => {
       return fuck(state, action)
     case 'GET_LIST_DATA':
       return getListData(state, action)
-    // case 'LEFT_CLICK':
-    //   return itemClick(state, action)
-    // case 'SHOW_CHOOSE_CONTENT':
-    //   return chooseContent(state, action)
-    // case 'CLEAR_CAR':
-    //   return clearCar(state, action)
     default:
       return state
   }
@@ -70,21 +64,3 @@ const getListData = (state, action) => {
     foodData: state.get('foodData').concat(fromJS(action.obj.data))
   })
 }
-
-// const chooseContent = (state, action) => {
-//   return state.merge({
-//     showChooseContent: state.get('showChooseContent').concat(fromJS(action.obj))
-//   })
-// }
-
-// const clearCar = (state) => {
-//   const listData = state.listData
-//   const list = listData.food_spu_tags || [] // 找到外层，左边 list 列表
-//   for (let i = 0; i < list.length; i++) {
-//     const spus = list[i].spus || []
-//     for (let j = 0; j < spus.length; j++) {
-//       spus[j].chooseCount = 0
-//     }
-//   }
-//   return { ...state, listData: JSON.parse(JSON.stringify(listData)) }
-// }
