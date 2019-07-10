c418dc66403aece0c635ba2429522220654ab6fc
GanYihuan <583520052@qq.com>
2019-07-10 11:59:03 +0800

    ready change bottombar styl

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,23 +8,44 @@ import ContentList from './components/ContentList/ContentList'
 import './Home.styl'
 
 class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ScrollY: 0
+    }
+  }
   render() {
     return (
-      <div className='home' ref='home'>
-        <div>
-          <Header/>
-          <Category/>
-          <Split/>
-          <ContentList/>
-          <BottomBar/>
+      <div>
+        <div className='home' ref='home'>
+          <div>
+            <Header/>
+            <Category/>
+            <Split/>
+            <ContentList/>
+            <div
+              className='back-to-ceiling'
+            >
+              <i className='icon-circle-up' />
+            </div>
+          </div>
         </div>
+        <BottomBar/>
       </div>
+
     )
   }
   componentDidMount() {
     if (!this.Scroll) {
       this.Scroll = new BScroll(this.refs.home, {
-        click: true
+        click: true,
+        probeType: 3
+      }).on('scroll', pos => {
+        this.setState(() => {
+          return {
+            ScrollY: pos.y
+          }
+        })
       })
     }
   }
