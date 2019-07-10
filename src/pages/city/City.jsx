78a441e789802f63e962f7754a0157b3d85278d8
GanYihuan <583520052@qq.com>
2019-07-10 21:25:00 +0800

    city header page

--- src/pages/city/City.jsx
+++ src/pages/city/City.jsx
@@ -1,13 +1,23 @@
 ﻿import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
+import './City.styl'
 
+@withRouter
 class City extends Component {
   render() {
     return (
-      <div className='header'>
-        city
+      <div className='City'>
+        <div className='header' onClick={() => this.goBack()}>
+          城市选择
+          <div className='iconfont header-back'>
+            <i className='icon-arrow_lift' />
+          </div>
+        </div>
       </div>
     )
+  }
+  goBack() {
+    this.props.history.push('/home')
   }
 }
 
