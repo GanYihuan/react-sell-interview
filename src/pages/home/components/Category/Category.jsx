8f4d01385c804cf2c273bcc5e111153f4df53bf1
GanYihuan <583520052@qq.com>
2019-07-10 10:19:05 +0800

    Category ready

--- src/pages/home/components/Category/Category.jsx
+++ src/pages/home/components/Category/Category.jsx
@@ -1,7 +1,7 @@
 ﻿import React, { Component } from 'react'
 import { connect } from 'react-redux' // 将 store 中的数据作为 props 绑定到组件上
-import Carousel from 'nuka-carousel' // 实现轮播图组件功能, 首页里的图标轮播组件
 import { actionCreators } from 'home/store/index'
+import Carousel from 'nuka-carousel' // 实现轮播图组件功能, 首页里的图标轮播组件
 import './Category.styl'
 
 class Category extends Component {
--- src/pages/home/components/Header/Header.jsx
+++ src/pages/home/components/Header/Header.jsx
@@ -5,6 +5,24 @@ class Header extends Component {
   render() {
     return (
       <div className='header'>
+        <div className='search-bar'>
+          <div className='bar-location'>
+            <div className='location-icon' />
+            <div className='location-text'>
+              city
+            </div>
+          </div>
+          <div
+            className='search-btn'
+            // @click="gotCity"
+          >
+            <input
+              className='place-holder'
+              type='text'
+              placeholder='搜索'
+            />
+          </div>
+        </div>
         <img className='banner-img' src='//xs01.meituan.net/waimai_i/img/bannertemp.e8a6fa63.jpg'/>
       </div>
     )
