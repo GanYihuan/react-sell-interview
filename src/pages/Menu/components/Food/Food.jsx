234f0db18c8fa2d25ddd4e7a90b3da4474d09cfb
GanYihuan <583520052@qq.com>
2019-07-15 16:53:20 +0800

    Food this.state ready

--- src/pages/Menu/components/Food/Food.jsx
+++ src/pages/Menu/components/Food/Food.jsx
@@ -1,6 +1,7 @@
 ﻿import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import moment from 'moment'
+import BScroll from 'better-scroll'
 import Split from 'Split/Split'
 import RatingSelect from '../RatingSelect/RatingSelect'
 import Control from '../Control/Control'
@@ -8,12 +9,19 @@ import { actionCreators } from '../Good/store'
 import './Food.styl'
 
 class Food extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showFood: false
+    }
+  }
   render() {
     const { commentData, ratingSelectType, foodData, showFood } = this.props
     const showFoods = showFood
     const commentDatas = commentData.toJS()
     const goodComment = commentDatas.filter(a => a.rateType === Math.max(0))
     const badComment = commentDatas.filter(a => a.rateType === Math.max(1))
+    const ratings = foodData.get('ratings').toJS()
     let showComment = []
     ratingSelectType === 1 ? showComment = badComment : ratingSelectType === 0 ? showComment = goodComment : showComment = commentDatas
     return (
@@ -23,6 +31,7 @@ class Food extends Component {
             ? <Fragment>
               <div
                 className='food'
+                ref='food'
               >
                 <div className='food-content'>
                   <div className='image-header'>
@@ -52,123 +61,77 @@ class Food extends Component {
                       >￥ {foodData.get('oldPrice')} </span>
                     </div>
                     {/* <div className='cartControl-wrapper'>
-                <Control/>
-              </div> */}
+                        <Control/>
+                      </div> */}
                     {/* <transition name="fade"> */}
                     {/* <div
-                className='buy'
-              >
-                加入购物车
-              </div> */}
+                        className='buy'
+                      >
+                        加入购物车
+                      </div> */}
                     {/* </transition> */}
-                  </div>
-                  <Split />
-                  <div
-                    className='info'
-                  >
-                    <h1 className='title'>
-                商品信息
-                    </h1>
-                    <p className='text'>
-                      {foodData.get('info')}
-                    </p>
                   </div>
                   <Split />
                   <div className='rating'>
                     <h1 className='title'>
-                商品评价
+                      商品评价
                     </h1>
                     <RatingSelect/>
                     <div className='rating-wrapper'>
-                      <ul >
-                        <li
-                          className='rating-item border-1px'
-                        >
-                          <div className='user'>
-                            <span className='name'>rating.username </span>
-                            <img
-                              className='avatar'
-                              alt='avatar'
-                            />
-                          </div>
-                          <div className='time'>
-                            {/* formatDate(rating.rateTime) */}
-                          </div>
-                          <p className='text'>
-                            {/* <i :className="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}" /> */}
-                      rating.text
-                          </p>
-                        </li>
+                      <ul>
+                        {
+                          ratings && ratings.length > 0
+                            ? ratings.map((item, index) => {
+                              return (
+                                <li
+                                  className='rating-item border-1px'
+                                  key={index}
+                                >
+                                  <div className='user'>
+                                    <span className='name'>{item.username}</span>
+                                    <img
+                                      className='avatar'
+                                      src={item.avatar}
+                                      alt='avatar'
+                                    />
+                                  </div>
+                                  <div className='time'>
+                                    {this.formatDate(item.rateTime)}
+                                  </div>
+                                  <div className='text'>
+                                    {
+                                      item.rateType === 0
+                                        ? <i className='icon-thumb_up' />
+                                        : <i className='icon-thumb_down' />
+                                    }
+                                    {item.text}
+                                  </div>
+                                </li>
+                              )
+                            })
+                            : <div
+                              className='no-rating'
+                            >
+                              暂无评价
+                            </div>
+                        }
                       </ul>
-                      <div
-                        className='no-rating'
-                      >
-                  暂无评价
-                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className='rating-wrapper border-1px'>
-                <ul>
-                  {
-                    showComment.map((item, index) => {
-                      return (
-                        <li
-                          className='rating-item'
-                          key={index}
-                        >
-                          <div className='avatar'>
-                            <img
-                              src={item.avatar}
-                              alt='avatar'
-                              width='28'
-                              height='28'
-                            />
-                          </div>
-                          <div className='content'>
-                            <h1 className='name'>
-                              {item.username}
-                            </h1>
-                            <div className='star-wrapper'>
-                              <StarScore score={item.score} size={24}/>
-                              <span
-                                className='delivery'
-                              >
-                                {item.deliveryTime}分钟送达
-                              </span>
-                            </div>
-                            <div className='text'>
-                              {item.text}
-                            </div>
-                            <div className='recommend'>
-                              {
-                                item.recommend.length > 0
-                                  ? <span className='icon-thumb_up' /> : null
-                              }
-                              {
-                                item.recommend.map((item, index) => {
-                                  return (
-                                    <span className='item' key={index}>{item}</span>
-                                  )
-                                })
-                              }
-                            </div>
-                            <div className='time'>
-                              {this.formatDate(item.rateTime)}
-                            </div>
-                          </div>
-                        </li>
-                      )
-                    })
-                  }
-                </ul>
               </div>
             </Fragment>
             : null
         }
       </Fragment>
     )
+  }
+  componentDidMount() {
+    if (!this.mScroll) {
+      this.mScroll = new BScroll(this.refs.food, {
+        click: true
+      })
+    }
   }
   hide() {
     const { dispathShowFood, showFood } = this.props
